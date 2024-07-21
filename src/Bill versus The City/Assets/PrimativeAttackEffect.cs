4d0f0b38036f9e3d8bc2086fa22e93e60f6bd015
Thomas Bonatti <thomasbonatti2695@gmail.com>
2024-07-21 11:49:30 -0400

    improved weapon effect

--- src/Bill versus The City/Assets/PrimativeAttackEffect.cs
+++ src/Bill versus The City/Assets/PrimativeAttackEffect.cs
@@ -8,8 +8,6 @@ public class PrimativeAttackEffect : MonoBehaviour
     public float max_scale = 0.1f;
     public float scale_rate = 1;
 
-    private bool scaling_down = false;
-
     void Start() {
         transform.localScale = new Vector3(start_scale, start_scale, start_scale);
     }
@@ -17,15 +15,8 @@ public class PrimativeAttackEffect : MonoBehaviour
     void Update() {
         float frame_scale = scale_rate * Time.deltaTime;
         Vector3 scale_vector = new Vector3(frame_scale, frame_scale, frame_scale);
-        if (scaling_down) {
-            scale_vector = -scale_vector;
-        }
-
         transform.localScale += scale_vector;
         if (transform.localScale.x >= max_scale) {
-            scaling_down = true;
-        }
-        if (scaling_down && transform.localScale.x <= start_scale) {
             Destroy(this.gameObject);
         }
 
