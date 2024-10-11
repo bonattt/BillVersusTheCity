c97ebc24f9aca338ee40cae1693c3b3e9d782a5a
Thomas Bonatti <thomasbonatti2695@gmail.com>
2024-10-11 12:30:30 -0400

    SORTA load the next scene

---
+++ src/Bill versus The City/Assets/Scripts/ClearEnemiesNextScene.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClearEnemiesNextScene : MonoBehaviour, IGenericObserver
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemiesManager.inst.Subscribe(this);   
+    }
+
+    public bool WinCondition() {
+        return EnemiesManager.inst.remaining_enemies == 0 && EnemiesManager.inst.total_enemies != 0;
+    }
+
+    void OnDestroy() {
+        EnemiesManager.inst.Unusubscribe(this);  
+    }
+    
+    public void UpdateObserver(IGenericObservable observable) {
+        if (WinCondition()) {
+            SceneManager.LoadScene("Demo001--level01");
+            Destroy(this);
+        }
+    }
+}
