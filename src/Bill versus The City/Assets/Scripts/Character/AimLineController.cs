62bb241a6e6d35aba18ddc262fe32cd1b46825fe
Thomas Bonatti <thomasbonatti2695@gmail.com>
2024-12-07 09:06:49 -0500

    aim line only renders while aiming

--- src/Bill versus The City/Assets/Scripts/Character/AimLineController.cs
+++ src/Bill versus The City/Assets/Scripts/Character/AimLineController.cs
@@ -12,6 +12,7 @@ public class AimLineController : MonoBehaviour
     private LineRenderer lineRenderer;
 
     public Color base_color = Color.red;
+    public PlayerAttackController attack_controller;
 
     // public LayerMask layer_mask;
 
@@ -32,21 +33,20 @@ public class AimLineController : MonoBehaviour
         Vector3 start_pos = PlayerCharacter.inst.player_object.transform.position;
         Vector3 base_end_pos = new Vector3(x, y_offset, z);
         
+        // get a unit vector in the direction of the mouse position
+        Vector3 unit_vector = (base_end_pos - start_pos).normalized;
 
+        // extend the unit vector from the mouse to `line_length`
+        Vector3 direction = unit_vector * line_length;
+        
         Vector3 end_pos;
-
         RaycastHit hit;
-        Vector3 direction = base_end_pos - start_pos;
         // if there is a wall in the way, don't draw aim-lines past it
         if (Physics.Raycast(start_pos, direction, out hit, direction.magnitude)) {
             end_pos = hit.point;
         } 
         else {
-            // get a unit vector in the direction of the mouse position
-            Vector3 unit_vector = (base_end_pos - start_pos).normalized;
-
-            // extend the unit vector from the mouse to `line_length`
-            end_pos = (unit_vector * line_length) + start_pos;
+            end_pos = direction + start_pos;
         }
 
         // int n = 10; // iterations of adding the line to itself to extend the line past the mouse
@@ -54,20 +54,23 @@ public class AimLineController : MonoBehaviour
         lineRenderer.SetPosition(0, start_pos); // Start
         lineRenderer.SetPosition(1, end_pos); // End
     }
-
     
-
     public Color GetColor() {
         // gets the color for this frame, including an alpha value based on current player aim.
-        float alpha = 1f; // TODO --- 
+        float alpha = attack_controller.aim_percent;
         return new Color(base_color.r, base_color.g, base_color.b, alpha);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        Color new_color = GetColor();
+        if (lineRenderer.material.color.a == 0 && new_color.a == 0) {
+            // optimization: if the line is not shown, and will still not be shown this frame, do not draw it
+            return;
+        }
+
         Vector3 mouse_pos = InputSystem.current.MouseWorldPosition();
         DrawLineTo(mouse_pos.x, mouse_pos.z);
-        lineRenderer.material.color = GetColor();
+        lineRenderer.material.color = new_color;
     }
 }
