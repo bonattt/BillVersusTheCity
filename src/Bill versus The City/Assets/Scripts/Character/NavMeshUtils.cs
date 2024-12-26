1cf0ea6288aaa9ede1d98ef0cdd5bc437f2e9ec9
Thomas Bonatti <thomasbonatti2695@gmail.com>
2024-12-26 11:31:14 -0500

    fixed Cover detection bug

--- src/Bill versus The City/Assets/Scripts/Character/NavMeshUtils.cs
+++ src/Bill versus The City/Assets/Scripts/Character/NavMeshUtils.cs
@@ -42,67 +42,67 @@ public static class NavMeshUtils {
     }
 
     
-    public static Vector3 GetRetreatWithCover(Vector3 start, Vector3 cover_from) {
-        int n_rays = 15;
-        return GetRetreatWithCover(start, cover_from, n_rays);
-    }
-    public static Vector3 GetRetreatWithCover(Vector3 start, Vector3 cover_from, int n_rays) {
-        start = new Vector3(start.x, 0.5f, start.z);
-        cover_from = new Vector3(cover_from.x, 0.5f, cover_from.z);
+    // public static Vector3 GetRetreatWithCover(Vector3 start, Vector3 cover_from) {
+    //     int n_rays = 15;
+    //     return GetRetreatWithCover(start, cover_from, n_rays);
+    // }
+    // public static Vector3 GetRetreatWithCover(Vector3 start, Vector3 cover_from, int n_rays) {
+    //     start = new Vector3(start.x, 0.5f, start.z);
+    //     cover_from = new Vector3(cover_from.x, 0.5f, cover_from.z);
+
+    //     bool start_has_cover = PositionHasCoverFrom(start, cover_from, draw_debug_ray: true);
+    //     if (start_has_cover) { return start; }
+
+    //     List<Vector3> cover_positions = GetCoverPositions(cover_from, n_rays);
+    //     if (cover_positions.Count == 0) {
+    //         return start + (5 * (cover_from - start).normalized); // if there are no cover positions, just try to move away
+    //     }
+    //     float best_distance_score = float.NegativeInfinity;
+    //     Vector3 best_position = cover_positions[0];
+    //     foreach (Vector3 position in cover_positions) {
+    //         float distance_score = CoverDistanceScore(position, start, cover_from);
+    //         if (distance_score > best_distance_score) {
+    //             best_distance_score = distance_score;
+    //             best_position = position;
+    //         }
+    //     }
+    //     return best_position;
+    // }
+
+    // private static float CoverDistanceScore(Vector3 destination, Vector3 start_pos, Vector3 cover_from) {
+    //     // returns a "score" for ranking the best destination point for an agent trying to move from `start_pos` to `destination` while 
+    //     // avoiding `cover_from`.
+    //     // method only factors distance.
+    //     float avoidance_score = Mathf.Sqrt(2 * Vector3.Distance(destination, cover_from)); // score for how far the point to be avoided is from the destination
+    //     float travel_score = Vector3.Distance(destination, cover_from); // score for how far the destination is from the start
         
-        bool start_has_cover = PositionHasCoverFrom(start, cover_from, draw_debug_ray: true);
-        if (start_has_cover) { return start; }
+    //     // high avoidance score is good, b/c we want to avoid, but high travel_score is bad b/c it means we have to travel a long way to reach safety
+    //     return avoidance_score - travel_score;  
+    // }
 
-        List<Vector3> cover_positions = GetCoverPositions(cover_from, n_rays);
-        if (cover_positions.Count == 0) {
-            return start + (5 * (cover_from - start).normalized); // if there are no cover positions, just try to move away
-        }
-        float best_distance_score = float.NegativeInfinity;
-        Vector3 best_position = cover_positions[0];
-        foreach (Vector3 position in cover_positions) {
-            float distance_score = CoverDistanceScore(position, start, cover_from);
-            if (distance_score > best_distance_score) {
-                best_distance_score = distance_score;
-                best_position = position;
-            }
-        }
-        return best_position;
-    }
-
-    private static float CoverDistanceScore(Vector3 destination, Vector3 start_pos, Vector3 cover_from) {
-        // returns a "score" for ranking the best destination point for an agent trying to move from `start_pos` to `destination` while 
-        // avoiding `cover_from`.
-        // method only factors distance.
-        float avoidance_score = Mathf.Sqrt(2 * Vector3.Distance(destination, cover_from)); // score for how far the point to be avoided is from the destination
-        float travel_score = Vector3.Distance(destination, cover_from); // score for how far the destination is from the start
-        
-        // high avoidance score is good, b/c we want to avoid, but high travel_score is bad b/c it means we have to travel a long way to reach safety
-        return avoidance_score - travel_score;  
-    }
-
-    private static List<Vector3> GetCoverPositions(Vector3 cover_from, int n_rays) {
-        // returns a list of Vector3's which have cover from `cover_from` AND are valid positions on a navmesh
-        List<Vector3> rays = GetNDirections(n_rays);
-        List<Vector3> valid_positions = new List<Vector3>();
-        foreach (Vector3 r in rays) {
-            if (Physics.Raycast(cover_from, r, out RaycastHit raycast_hit, float.PositiveInfinity)) {
-                if (! RaycastHitsPlayer(raycast_hit)) {
-                    float radius = 1f;
-                    Vector3 point_past_cover = raycast_hit.point + r.normalized * radius; // move `radius` units past the cover
-                    if (NavMesh.SamplePosition(point_past_cover, out NavMeshHit navmesh_hit, radius, NavMesh.AllAreas)) { 
-                        Vector3 valid_p = navmesh_hit.position;
-                        // retest that the navmesh position still has cover
-                        if (Physics.Raycast(cover_from, valid_p, out RaycastHit raycast_hit2, float.PositiveInfinity)) {
-                            if (! RaycastHitsPlayer(raycast_hit)) {
-                                valid_positions.Add(valid_p);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return valid_positions;
-    }
+    // private static List<Vector3> GetCoverPositions(Vector3 cover_from, int n_rays) {
+    //     // returns a list of Vector3's which have cover from `cover_from` AND are valid positions on a navmesh
+    //     List<Vector3> rays = GetNDirections(n_rays);
+    //     List<Vector3> valid_positions = new List<Vector3>();
+    //     foreach (Vector3 r in rays) {
+    //         if (Physics.Raycast(cover_from, r, out RaycastHit raycast_hit, float.PositiveInfinity)) {
+    //             if (! RaycastHitsPlayer(raycast_hit)) {
+    //                 float radius = 1f;
+    //                 Vector3 point_past_cover = raycast_hit.point + r.normalized * radius; // move `radius` units past the cover
+    //                 if (NavMesh.SamplePosition(point_past_cover, out NavMeshHit navmesh_hit, radius, NavMesh.AllAreas)) { 
+    //                     Vector3 valid_p = navmesh_hit.position;
+    //                     // retest that the navmesh position still has cover
+    //                     if (Physics.Raycast(cover_from, valid_p, out RaycastHit raycast_hit2, float.PositiveInfinity)) {
+    //                         if (! RaycastHitsPlayer(raycast_hit)) {
+    //                             valid_positions.Add(valid_p);
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return valid_positions;
+    // }
 
     public static bool RaycastHitsPlayer(RaycastHit hit) {
         // TODO --- find a better home for this helper method
@@ -116,11 +116,15 @@ public static class NavMeshUtils {
     }
 
     public static bool PositionHasCoverFrom(Vector3 cover_from, Vector3 end, bool draw_debug_ray = false) {
-        Vector3 raycast_direction = cover_from - end;
+        // move raycast points to position just above the ground to avoid treating the floor as cover
+        cover_from = new Vector3(cover_from.x, 1f, cover_from.z);
+        end = new Vector3(end.x, 1f, end.z);
+
+        Vector3 raycast_direction = end - cover_from;
         RaycastHit hit;
         float ray_length = raycast_direction.magnitude;
 
-        if (draw_debug_ray) { Debug.DrawRay(cover_from, raycast_direction, Color.cyan, ray_length); }
+        if (draw_debug_ray) { Debug.DrawRay(cover_from, raycast_direction, Color.cyan, Time.deltaTime); }
         if (Physics.Raycast(cover_from, raycast_direction, out hit, raycast_direction.magnitude)) {
             // if raycast hits something other than the player
             return !RaycastHitsPlayer(hit);
