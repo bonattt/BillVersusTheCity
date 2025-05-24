5dece5c163680acc444dc54018969d41fd336eaf
Thomas Bonatti <thomasbonatti2695@gmail.com>
2025-05-24 11:14:52 -0400

    fixed melee enemy

--- src/Bill versus The City/Assets/Scripts/Character/MeleeAttackController.cs
+++ src/Bill versus The City/Assets/Scripts/Character/MeleeAttackController.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 
 public class MeleeAttackController : MonoBehaviour, IAttackController
 {
-    public IWeapon current_weapon { get => current_melee; set { current_melee = (IMeleeWeapon)value; }}
-    public IFirearm current_gun {
+    public IWeapon current_weapon { get => current_melee; set { current_melee = (IMeleeWeapon)value; } }
+    public IFirearm current_gun
+    {
         get => null;
         set { throw new NotImplementedException(); }
     }
@@ -22,12 +23,14 @@ public class MeleeAttackController : MonoBehaviour, IAttackController
 
     public Transform _attack_start_point;
     public Transform attack_start_point { get => _attack_start_point; }
-    
+
     // set the script to be inactive while game is paused
     private bool _is_active = true;
-    public bool is_active {
+    public bool is_active
+    {
         get { return _is_active; }
-        set {
+        set
+        {
             // avoids shooting because you clicked on the last frame of dialouge
             _is_active = value;
         }
@@ -36,8 +39,8 @@ public class MeleeAttackController : MonoBehaviour, IAttackController
     // tracks when the last shot was fired, for handling rate-of-fire
     private float _last_attack_at = -10f;
     [SerializeField]
-    private Vector3 attack_direction; 
-    
+    private Vector3 attack_direction;
+
     public virtual int? current_slot
     {
         get { return null; }
@@ -46,7 +49,7 @@ public class MeleeAttackController : MonoBehaviour, IAttackController
     public void StartAim() { /* do nothing */ }
     public void StopAim() { /* do nothing */ }
     public float aim_percent { get => 0f; }
-    
+
     public bool CanAttack()
     {
         return current_melee != null && attack_stage == MeleeAttackStage.none && is_active;
@@ -54,6 +57,7 @@ public class MeleeAttackController : MonoBehaviour, IAttackController
 
     void Start()
     {
+        attacker = GetComponent<IAttackTarget>();
         current_melee = _init_weapon.CopyMeleeWeapon();
     }
 
@@ -67,6 +71,7 @@ public class MeleeAttackController : MonoBehaviour, IAttackController
             Debug.LogWarning("attacker is null!");
         }
         UpdateAttackInProgress();
+        UpdateDebug();
     }
 
     public MeleeAttackStage attack_stage = MeleeAttackStage.none;
@@ -75,32 +80,44 @@ public class MeleeAttackController : MonoBehaviour, IAttackController
         MeleeAttackStage next_stage = GetNextState();
         // if (attack_stage != next_stage) { Debug.LogWarning($"attack state {attack_stage} --> {next_stage}"); } // TODO --- remove debug
 
-        if (attack_stage == MeleeAttackStage.attack || next_stage == MeleeAttackStage.attack) {
+        if (attack_stage == MeleeAttackStage.attack || next_stage == MeleeAttackStage.attack)
+        {
             ResolveAttack();
         }
         attack_stage = next_stage;
-        // if (attack_stage == MeleeAttackStage.none)
-        // {
-        //     current_attack = null;
-        // }
+        if (current_attack != null && Time.time > _last_attack_at + current_attack.melee_weapon.total_attack_time) {
+            current_attack = null;
+        }
     }
 
-    private MeleeAttackStage GetNextState() {
+    private MeleeAttackStage GetNextState()
+    {
         float windup_until = _last_attack_at + current_melee.attack_windup;
         float attack_until = windup_until + current_melee.attack_duration;
         float recovery_until = attack_until + current_melee.attack_recovery;
         float cooldown_until = recovery_until + current_melee.attack_cooldown;
-        if (Time.time < _last_attack_at) {
+        if (Time.time < _last_attack_at)
+        {
             return MeleeAttackStage.none; // no attack started
-        } if (Time.time > _last_attack_at && Time.time < windup_until) {
-            return MeleeAttackStage.windup; 
-        } else if (Time.time > windup_until && Time.time < attack_until) {
+        }
+        if (Time.time > _last_attack_at && Time.time < windup_until)
+        {
+            return MeleeAttackStage.windup;
+        }
+        else if (Time.time > windup_until && Time.time < attack_until)
+        {
             return MeleeAttackStage.attack;
-        } else if (Time.time > attack_until && Time.time < recovery_until) {
+        }
+        else if (Time.time > attack_until && Time.time < recovery_until)
+        {
             return MeleeAttackStage.recovery;
-        } else if (Time.time > recovery_until && Time.time < cooldown_until) {
+        }
+        else if (Time.time > recovery_until && Time.time < cooldown_until)
+        {
             return MeleeAttackStage.cooldown;
-        } else if (Time.time > cooldown_until) {
+        }
+        else if (Time.time > cooldown_until)
+        {
             return MeleeAttackStage.none;
         }
         // Debug.LogError($"Unexpected attack timing state. time: {Time.time}, _last_attack_at: {_last_attack_at}");
@@ -113,13 +130,14 @@ public class MeleeAttackController : MonoBehaviour, IAttackController
         RaycastHit[] melee_hits = Physics.RaycastAll(attack_start_point.position, attack_direction, current_melee.attack_reach);
         Debug.LogWarning($"melee raycast through {melee_hits.Length} objects."); // TODO --- remove debug
         Debug.DrawRay(attack_start_point.position, attack_direction.normalized * current_melee.attack_reach, Color.green);
-        foreach (RaycastHit hit in melee_hits) {
+        foreach (RaycastHit hit in melee_hits)
+        {
             Debug.LogWarning($"Melee attack hits {hit.collider.gameObject.name}"); // TODO --- remove debug
             IAttackTarget hit_target = hit.collider.gameObject.GetComponent<IAttackTarget>();
             if (hit_target != null && !current_attack.hit_targets.Contains(hit_target))
             {
-                AttackResolver.ResolveAttackHit(current_attack, hit_target, hit.point);
                 current_attack.hit_targets.Add(hit_target);
+                AttackResolver.ResolveAttackHit(current_attack, hit_target, hit.point);
                 Debug.LogWarning($"melee ACTUALLY attack hit {hit_target}"); // TODO --- remove debug
             }
         }
@@ -129,7 +147,7 @@ public class MeleeAttackController : MonoBehaviour, IAttackController
     {
         // fires an attack with the current weapon
         if (
-            (_last_attack_at + total_attack_time <= Time.time)
+            (_last_attack_at + total_attack_time < Time.time)
             && (!InputSystem.IsNullPoint(new_attack_direction))
         )
         {
@@ -139,7 +157,8 @@ public class MeleeAttackController : MonoBehaviour, IAttackController
 
     private void _StartAttack(Vector3 new_attack_direction)
     {
-        current_attack = new MeleeAttack();
+        if (current_attack == null) current_attack = new MeleeAttack();
+        else Debug.LogError("attack made while current attack isn't nulled yet!");
         current_attack.melee_weapon = current_melee;
         current_attack.ignore_armor = false;
         current_attack.hit_targets.Add(attacker); // prevent from hitting self with melee attack
@@ -156,6 +175,15 @@ public class MeleeAttackController : MonoBehaviour, IAttackController
         effect.transform.rotation = transform.rotation;
         Debug.LogWarning("TODO --- melee attack hard codes visual effect!! should be an effect");
         // TODO ---
+    }
+
+    ////////// DEBUG //////////
+
+    public string debug__current_attack = "";
+    void UpdateDebug()
+    {
+        if (current_attack == null) { debug__current_attack = ""; }
+        else { debug__current_attack = $"attacl: {current_attack}"; }
     }
 }
 
