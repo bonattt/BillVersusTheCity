138925f3998f88d0a9a29ab464ad43538a42b523
Thomas Bonatti <thomasbonatti2695@gmail.com>
2025-06-23 12:51:56 -0400

    explosions blocked by walls

--- src/Bill versus The City/Assets/Scripts/Character/Attack/Explosion.cs
+++ src/Bill versus The City/Assets/Scripts/Character/Attack/Explosion.cs
@@ -7,6 +7,7 @@ public class Explosion : MonoBehaviour, IGameEventEffect
     public bool destroy_on_explode = true;
     private bool _exploded = false;
     public ExplosionAttack explosion_attack;
+    public Transform raycast_from;
 
     private bool _effect_completed = false;
     public bool effect_completed { get => _effect_completed; }
@@ -52,8 +53,8 @@ public class Explosion : MonoBehaviour, IGameEventEffect
 
     private void SpawnExplosionEffects()
     {
-        foreach (GameObject prefab in explosion_attack.explosion_effects)
-        {
+        Debug.LogWarning("TODO --- spawn explosion sound");
+        foreach (GameObject prefab in explosion_attack.explosion_effects) {
             GameObject effect = Instantiate(prefab);
             effect.transform.position = transform.position;
         }
@@ -79,46 +80,64 @@ public class Explosion : MonoBehaviour, IGameEventEffect
     }
 
     private IEnumerable<IAttackTarget> GetExplosionHits() {
-        Collider[] hits = Physics.OverlapSphere(transform.position, explosion_attack.explosion_radius);
+        Collider[] hits = Physics.OverlapSphere(raycast_from.position, explosion_attack.explosion_radius);
         foreach (Collider c in hits) {
             IAttackTarget target = c.gameObject.GetComponentInParent<IAttackTarget>();
             if (target != null) {
                 if (targets_hit.Contains(target)) {
                     continue; // target already hit, skip
                 } else if (ExplosionBlocked(target)) {
+                    // targets_hit.Add(target);
                     Debug.LogWarning($"Explosion blocked for target {target}!"); // TODO --- remove debug
                     continue; 
                 } else {
                     targets_hit.Add(target);
+                    // Debug.DrawLine(raycast_from.position, target.GetHitTarget().transform.position, Color.green, 10f); // TODO --- remove debug
                     yield return target;
                 }
             }
         }
     }
 
+    void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(raycast_from.position, explosion_attack.explosion_radius);
+    }
+
     public bool ExplosionBlocked(IAttackTarget target) {
         // returns a false if there is a wall protecting the target from the explosion
-        Vector3 to_target = target.GetHitTarget().transform.position - transform.position;
+        Vector3 target_position = target.GetHitTarget().transform.position;
+        Vector3 to_target = target_position - raycast_from.position;
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, to_target, out hit, explosion_attack.explosion_radius, blocks_explosion)) {
+        Debug.DrawRay(raycast_from.position, to_target, Color.green, 10f); // TODO --- remove debug
+        float raycast_length = Vector3.Distance(raycast_from.position, target_position);
+        if (Physics.Raycast(raycast_from.position, to_target, out hit, raycast_length, blocks_explosion)) {
             // if the raycast hit hit a game object sharing a hierarchy with the target, the target is not blocked.
-            return !GameObjectBelongsToTarget(hit.collider.gameObject, target);
+            // return !GameObjectBelongsToTarget(hit.collider.gameObject, target);
+            return true; // raycast layerMask should not include damageable targets...
         }
         // if the raycast hits nothing, the explosion isn't blocked
+        Debug.LogWarning("no raycast hit, target is not blocked"); // TODO --- remove debug
         return false;
     }
 
     public bool GameObjectBelongsToTarget(GameObject obj, IAttackTarget target) {
         // takes a GameObject and an IAttackTarget, and returns true if both are part of the same hierarchy.
         // if they are unrelated objects, return false.
+        Debug.LogWarning($"000: {obj.name} {target.GetHitTarget().name}"); // TODO --- remove debug
         IAttackTarget obj_target = obj.GetComponentInParent<IAttackTarget>();
-        if (obj_target == target) { return true; }
+        if (obj_target == target) { 
+            Debug.LogWarning($"111: {target.GetHitTarget().name}"); // TODO --- remove debug
+            return true; 
+        }
         else if (obj_target != null) {
+            Debug.LogWarning($"222: {target.GetHitTarget().name}"); // TODO --- remove debug
             return false; // if the obj has an AttackTarget, but it's NOT the same target, than the explosion is blocked by something
         } else {
             // if there is no attack target in parents, check children
             obj_target = obj.GetComponentInChildren<IAttackTarget>();
-            return obj_target == null || obj_target == target;
+            Debug.LogWarning($"333: {obj_target} == {target.GetHitTarget().name} -- `{obj_target == target}`"); // TODO --- remove debug
+            return obj_target == target;
         }
     }
 }
